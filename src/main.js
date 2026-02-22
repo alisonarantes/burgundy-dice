2aaf36ed3a16a9987f5064fd783d287c3b8b35f7
Alison Arantes <alisonarantes@gmail.com>
2026-02-22 18:03:31 -0300

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ const screens = {
 
 const i18n = {
     en: {
-        uiTitle: "The Castles of Burgundy: The Dice Game <span style='font-size: 0.8rem; color: var(--text-muted); font-weight: normal; margin-left: 10px;'>v1.12</span>",
+        uiTitle: "The Castles of Burgundy: The Dice Game <span style='font-size: 0.8rem; color: var(--text-muted); font-weight: normal; margin-left: 10px;'>v1.13</span>",
         menu: "☰ Menu",
         historyBtn: "Scores",
         rulesBtn: "Rules",
@@ -39,7 +39,7 @@ const i18n = {
         areaScoreTotal: "Total"
     },
     pt: {
-        uiTitle: "The Castles of Burgundy: O Jogo de Dados <span style='font-size: 0.8rem; color: var(--text-muted); font-weight: normal; margin-left: 10px;'>v1.12</span>",
+        uiTitle: "The Castles of Burgundy: O Jogo de Dados <span style='font-size: 0.8rem; color: var(--text-muted); font-weight: normal; margin-left: 10px;'>v1.13</span>",
         menu: "☰ Menu",
         historyBtn: "Pontuações",
         rulesBtn: "Regras",
